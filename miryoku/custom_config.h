6d9883de60421b785143f48f293fb52ae63c92c0
Elliot Ruddock <elliot@ruddock.nz>
2024-07-25 19:44:32 +1200

    Remove u_et

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,5 +9,5 @@
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_ET(U_UND, Z),    U_ET(U_CUT, X),    U_ET(U_COPY, C),   U_ET(U_PST, D),    &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_MT(U_UND, Z),    U_MT(U_CUT, X),    U_MT(U_COPY, C),   U_MT(U_PST, D),    &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC), U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),  U_LT(U_FUN, DEL),  U_NP,              U_NP
