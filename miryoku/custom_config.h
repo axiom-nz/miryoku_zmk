bc4f5f344dbde3b9fdca6f8b59783f2417fd5b2f
Elliot Ruddock <elliot@ruddock.nz>
2024-07-28 12:19:05 +1200

    workflow update

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,3 +4,9 @@
 
 #define MIRYOKU_CLIPBOARD_MAC
 #define MIRYOKU_KLUDGE_SOFT_OFF
+
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LCTRL, A),    U_MT(LALT, R),     U_MT(LGUI, S),     U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LGUI, E),     U_MT(LALT, I),     U_MT(LCTRL, O),    \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC), U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),  U_LT(U_FUN, DEL),  U_NP,              U_NP
