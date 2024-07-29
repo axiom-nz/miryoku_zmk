9d4a688d43cefb45bb90d2cc7972246d1715965c
Elliot Ruddock <elliot-ruddock@idexx.com>
2024-07-30 08:29:19 +1200

    Fix incorrect key for paste

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,5 +8,5 @@
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LCTRL, A),    U_MT(LALT, R),     U_MT(LGUI, S),     U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LGUI, E),     U_MT(LALT, I),     U_MT(LCTRL, O),    \
-U_ET(UE_UND, Z),   U_ET(UE_CUT, X),   U_ET(UE_CPY, C),   U_ET(UE_PST, C),   &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_ET(UE_UND, Z),   U_ET(UE_CUT, X),   U_ET(UE_CPY, C),   U_ET(UE_PST, D),   &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC), U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),  U_LT(U_FUN, DEL),  U_NP,              U_NP
